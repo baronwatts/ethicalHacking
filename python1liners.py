7f0effd5a1e85b0a558cd64eed217dbb4e47a642
Baron Watts <watts.baron@hotmail.com>
2024-05-02 23:01:03 -0500

    Update python1liners.py

--- python1liners.py
+++ python1liners.py
@@ -371,4 +371,30 @@ print(sha256(input_.encode('utf-8')).hexdigest())
 
 
 
+#==================================================================================
+# Scytale Cipher
+#==================================================================================
+plaintext = "Iryyatbhmvaehedlurlp"
+plaintext2 = "hdsioootsrwehwk"
+
+def encrypt(rows, plaintext):
+    assert len(plaintext) % rows == 0
+    n = len(plaintext)
+    columns = n // rows
+    ciphertext = ['-'] * n
+    for i in range(n):
+        row, col = i // columns, i % columns
+        ciphertext[col * rows + row] = plaintext[i]
+    return "".join(ciphertext)
+
+def decrypt(rows, ciphertext):
+    assert len(ciphertext) % rows == 0
+    return encrypt(len(ciphertext) // rows, ciphertext)
+    
+       
+print(encrypt(3, plaintext2)) #=> howdoesthiswork
+
+
+
+
 
